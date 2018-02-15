1f40491d56c81c492b98c3f1d30cc625091e823c
gta5051170 <gta5051170@gmail.com>
2018-02-15 17:52:27 -0500

    999

---
+++ workspace/D213.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class D213{
+    public static void main(String [] args) {
+    Scanner ipt = new Scanner(System.in);
+    System.out.print("your name(no space containing):");
+    
+    String name = ipt.next();
+    System.out.println("which level you want you want to play?");
+    System.out.println("");
+    System.out.println(" 1 easy");
+    System.out.println(" 2 hard");
+    System.out.println(" 3 superhard");
+    System.out.println("");
+    System.out.println("choose 1/2 :");
+    String level = ipt.next();
+    
+    System.out.println("---------------------------");
+    System.out.println("player's info");
+    System.out.println("--------------------------");
+    System.out.println("");
+    System.out.println("name :" + name);
+    System.out.println("level:" + level);
+      
+    
+     
+    
+    
+    
+    
+    
+    }
+    
+           
+    
+    
+   }
+   
+    
+    
+ 
--- workspace/demoboolean.java
+++ workspace/demoboolean.java
@@ -1,5 +1,37 @@
 public class demoboolean{
-   public static void main(string[] args)
+   public static void main(string[] args) {
+     
+     int num = 1;
+     num = num + 1; 
+     num++;
+     ++num;
+     
+     num+1;
+     int count = ++num;
+     system.out.println("count" + count + "num" + num);
+     num= 1;
+     count = num ++;
+     system.out.println("count" + count + "num" + num);
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+   }
 
 
 
